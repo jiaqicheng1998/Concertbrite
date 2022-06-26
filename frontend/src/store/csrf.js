901a718a017bbfeb1111ec901f56df8b839a95cd
jiaqicheng1998 <jiaqicheng1998@gmail.com>
2022-06-25 18:26:15 -0700

    fisrt commit

---
+++ frontend/src/store/csrf.js
@@ -0,0 +1,23 @@
+import Cookies from 'js-cookie';
+
+export function restoreCSRF() {
+    return csrfFetch('/api/csrf/restore');
+}
+
+export async function csrfFetch(url, options = {}) {
+    options.method = options.method || 'GET';
+    options.headers = options.headers || {};
+
+    if (options.method.toUpperCase() !== 'GET') {
+        options.headers['Content-Type'] =
+            options.headers['Content-Type'] || 'application/json';
+        options.headers['XSRF-Token'] = Cookies.get('XSRF-TOKEN')
+    }
+
+    const res = await window.fetch(url, options);
+
+    if (res.status >= 400) throw res;
+    return res;
+}
+
+export default csrfFetch;
---
+++ frontend/src/store/session.js
@@ -0,0 +1,88 @@
+//Session actions and reducer
+import { csrfFetch } from './csrf';
+
+//Their types should be extracted as a constant
+const SET_USER = 'session/setUser';
+const REMOVE_USER = 'session/removeUser';
+
+//Create two POJO action creators. 
+const setUser = (user) => {
+    return {
+        type: SET_USER,
+        payload: user,
+    };
+};
+
+const removeUser = () => {
+    return {
+        type: REMOVE_USER,
+    };
+};
+
+//add a thunk action for the POST /api/session
+export const login = (user) => async (dispatch) => {
+    const { credential, password } = user;
+    const response = await csrfFetch('/api/session', {
+        method: 'POST',
+        body: JSON.stringify({
+            credential,
+            password,
+        }),
+    });
+
+    const data = await response.json();
+    dispatch(setUser(data.user));
+    return response;
+}
+
+export const restoreUser = () => async dispatch => {
+    const response = await csrfFetch('/api/session');
+    const data = await response.json();
+    dispatch(setUser(data.user));
+    return response;
+}
+
+export const signup = (user) => async (dispatch) => {
+    const { username, email, password } = user;
+    const response = await csrfFetch("/api/users", {
+        method: "POST",
+        body: JSON.stringify({
+            username,
+            email,
+            password,
+        }),
+    });
+
+    const data = await response.json();
+    dispatch(setUser(data.user));
+    return response;
+};
+
+export const logout = () => async (dispatch) => {
+    const response = await csrfFetch('/api/session', {
+        method: 'DELETE',
+    });
+    dispatch(removeUser());
+    return response;
+}
+
+//export the reducer as the default export.
+const initialState = { user: null };
+
+const sessionReducer = (state = initialState, action) => {
+    let newState;
+    switch (action.type) {
+        case SET_USER:
+            newState = Object.assign({}, state);
+            newState.user = action.payload;
+            return newState;
+        case REMOVE_USER:
+            newState = Object.assign({}, state);
+            newState.user = null;
+            return newState;
+        default:
+            return state;
+    }
+};
+
+export default sessionReducer;
